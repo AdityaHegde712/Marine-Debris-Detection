aba145652ffa3a0253c67d65146e9bb07feaeebd
Lyra0710 <ananyayallapragada@gmail.com>
2025-03-16 08:56:20 +0530

    Web Changes

--- Web/FE/src/App.js
+++ Web/FE/src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import ObjectDetection from './pages/ObjectDetection'; // Import as default, no curly braces
 import { HomePage } from './pages/HomePage';
-import PredictFire from './pages/PredictFire';
 import { Models } from './components/Models';
 import Sentinel from './pages/Sentinel';
 function App() {
@@ -13,7 +12,6 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/object-detection" element={<ObjectDetection />} />
         <Route path="/sentinel" element={<Sentinel />} />
-        <Route path ="/predict-fire" element={<PredictFire />} />
         <Route path="/homepage" element={<HomePage />} />
       </Routes>
     </Router>
